76018d372d7f14b84262a06a67d1c6e845f1153b
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-10-20 13:59:33 -0300

    refactor: improve formatting of cast function for consistency

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,9 @@
 <?php
 
 if (! function_exists('cast')) {
-    function cast() {
-        return new class {
+    function cast()
+    {
+        return new class () {
             public function toJsonPretty(array $data): string
             {
                 return json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
