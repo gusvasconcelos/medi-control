2953b4b129acd04115f80dab9e624ff80bb04622
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-10-23 12:48:49 -0300

    fix: change require_once to require in routes to prevent route loading issues on machine restart

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'v1'
 ], function () {
-    require_once __DIR__ . '/api/auth.php';
-    require_once __DIR__ . '/api/user-medications.php';
+    require __DIR__ . '/api/auth.php';
+    require __DIR__ . '/api/user-medications.php';
 });
