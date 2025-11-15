8a903655da0056859791851f4afa55790d497753
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-11-15 11:23:25 -0300

    feat: add migration to index interactions column in medications table
    - Created a new migration to alter the interactions column type to jsonb.
    - Added GIN indexes for improved query performance on interactions, medication IDs, and classes.

---
+++ database/migrations/2025_11_15_110838_add_index_to_medications_interactions_column.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+{
+    DB::statement('ALTER TABLE medications ALTER COLUMN interactions TYPE jsonb USING interactions::jsonb');
+
+    DB::statement("CREATE INDEX idx_medications_interactions_gin ON medications USING GIN (interactions jsonb_path_ops)");
+    DB::statement("CREATE INDEX idx_interactions_medication_id ON medications USING GIN ((interactions -> 'medications'))");
+    DB::statement("CREATE INDEX idx_interactions_classes ON medications USING GIN ((interactions -> 'classes'))");
+}
+
+public function down(): void
+    {
+        DB::statement('DROP INDEX IF EXISTS idx_medications_interactions_gin');
+        DB::statement('DROP INDEX IF EXISTS idx_interactions_medication_id');
+        DB::statement('DROP INDEX IF EXISTS idx_interactions_classes');
+
+        DB::statement('ALTER TABLE medications ALTER COLUMN interactions TYPE json USING interactions::json');
+    }
+};
