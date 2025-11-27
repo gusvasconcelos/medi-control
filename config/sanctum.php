5929e3c1db925e185fd3e9fe4bbf4165f2fd393f
Gustavo Vasconcelos <gusvasconcelos@proton.me>
2025-11-26 22:24:20 -0300

    fix: update stateful domains in Sanctum configuration
    - Added 'medi-control.up.railway.app' to the list of stateful domains in the Sanctum configuration to support additional application URLs for session management.

--- config/sanctum.php
+++ config/sanctum.php
@@ -16,8 +16,9 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
+        '%s%s%s%s',
         'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,::1',
+        ',medi-control.up.railway.app',
         Sanctum::currentApplicationUrlWithPort() ? ',' . Sanctum::currentApplicationUrlWithPort() : '',
         env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : '',
     ))),
